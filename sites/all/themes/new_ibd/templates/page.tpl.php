917e9a21f896bf945f79f7d4a03ef3d6dde8e460
JGrubb <therealjohnnygrubb@gmail.com>
2011-06-07 10:46:34 -0400

    removed 2 lines from page.tpl

--- sites/all/themes/new_ibd/templates/page.tpl.php
+++ sites/all/themes/new_ibd/templates/page.tpl.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
   <head>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php print $head ?>
     <?php print $styles ?>
     <title><?php print $head_title ?></title>
