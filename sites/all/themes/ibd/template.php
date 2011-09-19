ae4ad864252e813ad48e26ade1ffb4ae163c4f2e
JGrubb <therealjohnnygrubb@gmail.com>
2011-09-19 13:31:56 -0400

    jekyllification

--- sites/all/themes/ibd/template.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function ibd_node_submitted($node) {
-  return t('!username . @datetime',
-    array(
-      '!username' => theme('username', $node),
-      '@datetime' => format_date($node->created),
-    ));
-}
