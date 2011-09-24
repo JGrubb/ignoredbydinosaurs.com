36054e519a27e3fe695d3364cefe8b5ce5a0ca0a
JGrubb <therealjohnnygrubb@gmail.com>
2011-09-24 14:38:38 -0400

    more GA

--- sites/all/themes/new_ibd/templates/page.tpl.php
+++ sites/all/themes/new_ibd/templates/page.tpl.php
@@ -5,19 +5,13 @@
     <?php print $styles ?>
     <title><?php print $head_title ?></title>
     <meta name=viewport content="width=device-width,initial-scale=1">
-    <script type="text/javascript">
-
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-8646459-1']);
-      _gaq.push(['_trackPageview']);
-
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-
-    </script>
+    <script>
+        var _gaq=[['_setAccount','UA-8646459-1'],['_trackPageview'],['_trackPageLoadTime']];
+        (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+        g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+        s.parentNode.insertBefore(g,s)}(document,'script'));
+      </script>
+    
   </head>
   <body <?php print drupal_attributes($attr) ?>>
 
