852a8fdef81512c2bbd53d079fa549025a46d0c6
JGrubb <therealjohnnygrubb@gmail.com>
2011-09-25 08:49:54 -0400

    h2's

--- sites/all/themes/new_ibd/js/scripts.js
+++ sites/all/themes/new_ibd/js/scripts.js
@@ -413,4 +413,28 @@ jQuery(document).ready(function($) {
   $("img").removeAttr("height").removeAttr("width");
 });
 
-
+var $allVideos = $("iframe[src^='http://www.youtube.com']"),
+    // The element that is fluid width
+    $fluidEl = $("article section");
+ 
+// Figure out and save aspect ratio for each video
+$allVideos.each(function() {
+  $(this)
+    .data('aspectRatio', this.height / this.width)
+    // and remove the hard coded width/height
+    .removeAttr('height')
+    .removeAttr('width');
+});
+ 
+// When the window is resized
+$(window).resize(function() {
+  var newWidth = $fluidEl.width();
+  // Resize all videos according to their own aspect ratio
+  $allVideos.each(function() {
+    var $el = $(this);
+    $el
+      .width(newWidth)
+      .height(newWidth * $el.data('aspectRatio'));
+  });
+// Kick off one resize to fix all videos on page load
+}).resize();
